abcijkxyz/SSVM

[Common] Add error codes and error info of validation.

include/common/errcode.h
@@ -46,53 +46,93 @@ enum class ErrCode : uint8_t {
   WrongVMWorkflow = 0x03,   /// Wrong VM's workflow
   FuncNotFound = 0x04,      /// Wasm function not found
   /// Load phase
-  InvalidPath = 0x10,    /// File not found
-  ReadError = 0x11,      /// Error when reading
-  EndOfFile = 0x12,      /// Reach end of file when reading
-  InvalidGrammar = 0x13, /// Parsing error
-  InvalidVersion = 0x14, /// Unsupported version
+  InvalidPath = 0x20,    /// File not found
+  ReadError = 0x21,      /// Error when reading
+  EndOfFile = 0x22,      /// Reach end of file when reading
+  InvalidGrammar = 0x23, /// Parsing error
+  InvalidVersion = 0x24, /// Unsupported version
   /// Validation phase
-  ValidationFailed = 0x20, /// Validation checking failed
+  InvalidAlignment = 0x40,   /// Alignment > natural
+  TypeCheckFailed = 0x41,    /// Got unexpected type when checking
+  InvalidLabelIdx = 0x42,    /// Branch to unknown label index
+  InvalidLocalIdx = 0x43,    /// Access unknown local index
+  InvalidFuncTypeIdx = 0x44, /// Type index not defined
+  InvalidFuncIdx = 0x45,     /// Function index not defined
+  InvalidTableIdx = 0x46,    /// Table index not defined
+  InvalidMemoryIdx = 0x47,   /// Memory index not defined
+  InvalidGlobalIdx = 0x48,   /// Global index not defined
+  ConstExprRequired = 0x49,  /// Should be constant expression
+  DupExportName = 0x4A,      /// Export name conflicted
+  ImmutableGlobal = 0x4B,    /// Tried to store to const global value
+  MultiTables = 0x4C,        /// #Tables > 1
+  MultiMemories = 0x4D,      /// #Memories > 1
+  InvalidLimit = 0x4F,       /// Invalid Limit grammar
+  InvalidMemPages = 0x50,    /// Memory pages > 65536
+  InvalidStartFunc = 0x51,   /// Invalid start function signature
+  ValidationFailed = 0x5F,   /// Validation checking failed
   /// Instantiation phase
-  ModuleNameConflict = 0x30,     /// Module name conflicted when importing.
-  IncompatibleImportType = 0x31, /// Import matching failed
-  UnknownImport = 0x32,          /// Unknown import instances
-  DataSegDoesNotFit = 0x33,      /// Init failed when instantiating data segment
-  ElemSegDoesNotFit = 0x34, /// Init failed when instantiating element segment
+  ModuleNameConflict = 0x60,     /// Module name conflicted when importing.
+  IncompatibleImportType = 0x61, /// Import matching failed
+  UnknownImport = 0x62,          /// Unknown import instances
+  DataSegDoesNotFit = 0x63,      /// Init failed when instantiating data segment
+  ElemSegDoesNotFit = 0x64, /// Init failed when instantiating element segment
   /// Execution phase
-  WrongInstanceAddress = 0x40, /// Wrong access of instances addresses
-  WrongInstanceIndex = 0x41,   /// Wrong access of instances indices
-  InstrTypeMismatch = 0x42,    /// Instruction type not match
-  FuncSigMismatch = 0x43,      /// Function signature not match when invoking
-  DivideByZero = 0x44,         /// Divide by zero
-  IntegerOverflow = 0x45,      /// Integer overflow
-  InvalidConvToInt = 0x46,     /// Cannot do convert to integer
-  MemoryOutOfBounds = 0x47,    /// Out of bounds memory access
-  Unreachable = 0x48,          /// Meet an unreachable instruction
-  UninitializedElement = 0x49, /// Uninitialized element in table instance
-  UndefinedElement = 0x4A,     /// Access undefined element in table instances
-  IndirectCallTypeMismatch = 0x4B, /// Func type mismatch in call_indirect
-  ExecutionFailed = 0x4C           /// Host function execution failed
+  WrongInstanceAddress = 0x80, /// Wrong access of instances addresses
+  WrongInstanceIndex = 0x81,   /// Wrong access of instances indices
+  InstrTypeMismatch = 0x82,    /// Instruction type not match
+  FuncSigMismatch = 0x83,      /// Function signature not match when invoking
+  DivideByZero = 0x84,         /// Divide by zero
+  IntegerOverflow = 0x85,      /// Integer overflow
+  InvalidConvToInt = 0x86,     /// Cannot do convert to integer
+  MemoryOutOfBounds = 0x87,    /// Out of bounds memory access
+  Unreachable = 0x88,          /// Meet an unreachable instruction
+  UninitializedElement = 0x89, /// Uninitialized element in table instance
+  UndefinedElement = 0x8A,     /// Access undefined element in table instances
+  IndirectCallTypeMismatch = 0x8B, /// Func type mismatch in call_indirect
+  ExecutionFailed = 0x8C           /// Host function execution failed
 };
 
 /// Error code enumeration string mapping.
 static inline std::unordered_map<ErrCode, std::string> ErrCodeStr = {
+    /// SSVM runtime
     {ErrCode::Success, "success"},
     {ErrCode::Terminated, "terminated"},
     {ErrCode::CostLimitExceeded, "cost limit exceeded"},
     {ErrCode::WrongVMWorkflow, "wrong VM workflow"},
     {ErrCode::FuncNotFound, "wasm function not found"},
+    /// Load phase
     {ErrCode::InvalidPath, "invalid path"},
     {ErrCode::ReadError, "read error"},
     {ErrCode::EndOfFile, "read end of file"},
     {ErrCode::InvalidGrammar, "invalid wasm grammar"},
     {ErrCode::InvalidVersion, "invalid version"},
+    /// Validation phase
+    {ErrCode::InvalidAlignment, "alignment must not be larger than natural"},
+    {ErrCode::TypeCheckFailed, "type mismatch"},
+    {ErrCode::InvalidLabelIdx, "unknown label"},
+    {ErrCode::InvalidLocalIdx, "unknown local"},
+    {ErrCode::InvalidFuncTypeIdx, "unknown type"},
+    {ErrCode::InvalidFuncIdx, "unknown function"},
+    {ErrCode::InvalidTableIdx, "unknown table"},
+    {ErrCode::InvalidMemoryIdx, "unknown memory"},
+    {ErrCode::InvalidGlobalIdx, "unknown global"},
+    {ErrCode::ConstExprRequired, "constant expression required"},
+    {ErrCode::DupExportName, "duplicate export name"},
+    {ErrCode::ImmutableGlobal, "global is immutable"},
+    {ErrCode::MultiTables, "multiple tables"},
+    {ErrCode::MultiMemories, "multiple memories"},
+    {ErrCode::InvalidLimit, "size minimum must not be greater than maximum"},
+    {ErrCode::InvalidMemPages,
+     "memory size must be at most 65536 pages (4GiB)"},
+    {ErrCode::InvalidStartFunc, "start function"},
     {ErrCode::ValidationFailed, "validation failed"},
+    /// Instantiation phase
     {ErrCode::ModuleNameConflict, "module name conflict"},
     {ErrCode::IncompatibleImportType, "incompatible import type"},
     {ErrCode::UnknownImport, "unknown import"},
     {ErrCode::DataSegDoesNotFit, "data segment does not fit"},
     {ErrCode::ElemSegDoesNotFit, "elements segment does not fit"},
+    /// Execution phase
     {ErrCode::WrongInstanceAddress, "wrong instance address"},
     {ErrCode::WrongInstanceIndex, "wrong instance index"},
     {ErrCode::InstrTypeMismatch, "instruction type mismatch"},
@@ -108,7 +148,7 @@ static inline std::unordered_map<ErrCode, std::string> ErrCodeStr = {
     {ErrCode::ExecutionFailed, "host function failed"}};
 
 static inline WasmPhase getErrCodePhase(ErrCode Code) {
-  return static_cast<WasmPhase>((static_cast<uint8_t>(Code) & 0xF0) >> 4);
+  return static_cast<WasmPhase>((static_cast<uint8_t>(Code) & 0xF0) >> 5);
 }
 
 static inline std::ostream &operator<<(std::ostream &OS, ErrCode Code) {

qdrvm/WasmEdge

include/common/errcode.h
@@ -46,53 +46,93 @@ enum class ErrCode : uint8_t {
   WrongVMWorkflow = 0x03,   /// Wrong VM's workflow
   FuncNotFound = 0x04,      /// Wasm function not found
   /// Load phase
-  InvalidPath = 0x10,    /// File not found
-  ReadError = 0x11,      /// Error when reading
-  EndOfFile = 0x12,      /// Reach end of file when reading
-  InvalidGrammar = 0x13, /// Parsing error
-  InvalidVersion = 0x14, /// Unsupported version
+  InvalidPath = 0x20,    /// File not found
+  ReadError = 0x21,      /// Error when reading
+  EndOfFile = 0x22,      /// Reach end of file when reading
+  InvalidGrammar = 0x23, /// Parsing error
+  InvalidVersion = 0x24, /// Unsupported version
   /// Validation phase
-  ValidationFailed = 0x20, /// Validation checking failed
+  InvalidOpCode = 0x40,      /// Invalid instruction type
+  InvalidAlignment = 0x41,   /// Alignment > natural
+  TypeCheckFailed = 0x42,    /// Got unexpected type when checking
+  InvalidLabelIdx = 0x43,    /// Branch to unknown label index
+  InvalidLocalIdx = 0x44,    /// Access unknown local index
+  InvalidFuncTypeIdx = 0x45, /// Type index not defined
+  InvalidFuncIdx = 0x46,     /// Function index not defined
+  InvalidTableIdx = 0x47,    /// Table index not defined
+  InvalidMemoryIdx = 0x48,   /// Memory index not defined
+  InvalidGlobalIdx = 0x49,   /// Global index not defined
+  ConstExprRequired = 0x4A,  /// Should be constant expression
+  DupExportName = 0x4B,      /// Export name conflicted
+  ImmutableGlobal = 0x4C,    /// Tried to store to const global value
+  MultiTables = 0x4D,        /// #Tables > 1
+  MultiMemories = 0x4E,      /// #Memories > 1
+  InvalidLimit = 0x4F,       /// Invalid Limit grammar
+  InvalidMemPages = 0x50,    /// Memory pages > 65536
+  InvalidStartFunc = 0x51,   /// Invalid start function signature
   /// Instantiation phase
-  ModuleNameConflict = 0x30,     /// Module name conflicted when importing.
-  IncompatibleImportType = 0x31, /// Import matching failed
-  UnknownImport = 0x32,          /// Unknown import instances
-  DataSegDoesNotFit = 0x33,      /// Init failed when instantiating data segment
-  ElemSegDoesNotFit = 0x34, /// Init failed when instantiating element segment
+  ModuleNameConflict = 0x60,     /// Module name conflicted when importing.
+  IncompatibleImportType = 0x61, /// Import matching failed
+  UnknownImport = 0x62,          /// Unknown import instances
+  DataSegDoesNotFit = 0x63,      /// Init failed when instantiating data segment
+  ElemSegDoesNotFit = 0x64, /// Init failed when instantiating element segment
   /// Execution phase
-  WrongInstanceAddress = 0x40, /// Wrong access of instances addresses
-  WrongInstanceIndex = 0x41,   /// Wrong access of instances indices
-  InstrTypeMismatch = 0x42,    /// Instruction type not match
-  FuncSigMismatch = 0x43,      /// Function signature not match when invoking
-  DivideByZero = 0x44,         /// Divide by zero
-  IntegerOverflow = 0x45,      /// Integer overflow
-  InvalidConvToInt = 0x46,     /// Cannot do convert to integer
-  MemoryOutOfBounds = 0x47,    /// Out of bounds memory access
-  Unreachable = 0x48,          /// Meet an unreachable instruction
-  UninitializedElement = 0x49, /// Uninitialized element in table instance
-  UndefinedElement = 0x4A,     /// Access undefined element in table instances
-  IndirectCallTypeMismatch = 0x4B, /// Func type mismatch in call_indirect
-  ExecutionFailed = 0x4C           /// Host function execution failed
+  WrongInstanceAddress = 0x80, /// Wrong access of instances addresses
+  WrongInstanceIndex = 0x81,   /// Wrong access of instances indices
+  InstrTypeMismatch = 0x82,    /// Instruction type not match
+  FuncSigMismatch = 0x83,      /// Function signature not match when invoking
+  DivideByZero = 0x84,         /// Divide by zero
+  IntegerOverflow = 0x85,      /// Integer overflow
+  InvalidConvToInt = 0x86,     /// Cannot do convert to integer
+  MemoryOutOfBounds = 0x87,    /// Out of bounds memory access
+  Unreachable = 0x88,          /// Meet an unreachable instruction
+  UninitializedElement = 0x89, /// Uninitialized element in table instance
+  UndefinedElement = 0x8A,     /// Access undefined element in table instances
+  IndirectCallTypeMismatch = 0x8B, /// Func type mismatch in call_indirect
+  ExecutionFailed = 0x8C           /// Host function execution failed
 };
 
 /// Error code enumeration string mapping.
 static inline std::unordered_map<ErrCode, std::string> ErrCodeStr = {
+    /// SSVM runtime
     {ErrCode::Success, "success"},
     {ErrCode::Terminated, "terminated"},
     {ErrCode::CostLimitExceeded, "cost limit exceeded"},
     {ErrCode::WrongVMWorkflow, "wrong VM workflow"},
     {ErrCode::FuncNotFound, "wasm function not found"},
+    /// Load phase
     {ErrCode::InvalidPath, "invalid path"},
     {ErrCode::ReadError, "read error"},
     {ErrCode::EndOfFile, "read end of file"},
     {ErrCode::InvalidGrammar, "invalid wasm grammar"},
     {ErrCode::InvalidVersion, "invalid version"},
-    {ErrCode::ValidationFailed, "validation failed"},
+    /// Validation phase
+    {ErrCode::InvalidOpCode, "invalid instruction opcode"},
+    {ErrCode::InvalidAlignment, "alignment must not be larger than natural"},
+    {ErrCode::TypeCheckFailed, "type mismatch"},
+    {ErrCode::InvalidLabelIdx, "unknown label"},
+    {ErrCode::InvalidLocalIdx, "unknown local"},
+    {ErrCode::InvalidFuncTypeIdx, "unknown type"},
+    {ErrCode::InvalidFuncIdx, "unknown function"},
+    {ErrCode::InvalidTableIdx, "unknown table"},
+    {ErrCode::InvalidMemoryIdx, "unknown memory"},
+    {ErrCode::InvalidGlobalIdx, "unknown global"},
+    {ErrCode::ConstExprRequired, "constant expression required"},
+    {ErrCode::DupExportName, "duplicate export name"},
+    {ErrCode::ImmutableGlobal, "global is immutable"},
+    {ErrCode::MultiTables, "multiple tables"},
+    {ErrCode::MultiMemories, "multiple memories"},
+    {ErrCode::InvalidLimit, "size minimum must not be greater than maximum"},
+    {ErrCode::InvalidMemPages,
+     "memory size must be at most 65536 pages (4GiB)"},
+    {ErrCode::InvalidStartFunc, "start function"},
+    /// Instantiation phase
     {ErrCode::ModuleNameConflict, "module name conflict"},
     {ErrCode::IncompatibleImportType, "incompatible import type"},
     {ErrCode::UnknownImport, "unknown import"},
     {ErrCode::DataSegDoesNotFit, "data segment does not fit"},
     {ErrCode::ElemSegDoesNotFit, "elements segment does not fit"},
+    /// Execution phase
     {ErrCode::WrongInstanceAddress, "wrong instance address"},
     {ErrCode::WrongInstanceIndex, "wrong instance index"},
     {ErrCode::InstrTypeMismatch, "instruction type mismatch"},
@@ -108,7 +148,7 @@ static inline std::unordered_map<ErrCode, std::string> ErrCodeStr = {
     {ErrCode::ExecutionFailed, "host function failed"}};
 
 static inline WasmPhase getErrCodePhase(ErrCode Code) {
-  return static_cast<WasmPhase>((static_cast<uint8_t>(Code) & 0xF0) >> 4);
+  return static_cast<WasmPhase>((static_cast<uint8_t>(Code) & 0xF0) >> 5);
 }
 
 static inline std::ostream &operator<<(std::ostream &OS, ErrCode Code) {