sshyran/SSVM

[Interpreter] Implementation of new bulk-memory and reference-type instructions. 1. Reference instructions: * ref.null * ref.is_null * ref.func 2. Table instructions: * table.get * table.set * table.init * elem.drop * table.copy * table.grow * table.size * table.fill 3. Memory instructions: * memory.init * data.drop * memory.copy * memory.fill

include/interpreter/interpreter.h
@@ -244,6 +244,14 @@ private:
   /// Helper function for get global instance by index.
   Runtime::Instance::GlobalInstance *
   getGlobInstByIdx(Runtime::StoreManager &StoreMgr, const uint32_t Idx);
+
+  /// Helper function for get element instance by index.
+  Runtime::Instance::ElementInstance *
+  getElemInstByIdx(Runtime::StoreManager &StoreMgr, const uint32_t Idx);
+
+  /// Helper function for get data instance by index.
+  Runtime::Instance::DataInstance *
+  getDataInstByIdx(Runtime::StoreManager &StoreMgr, const uint32_t Idx);
   /// @}
 
   /// \name Run instructions functions
@@ -274,6 +282,22 @@ private:
                               const uint32_t Idx);
   Expect<void> runGlobalSetOp(Runtime::StoreManager &StoreMgr,
                               const uint32_t Idx);
+  /// ======= Table instructions =======
+  Expect<void> runTableGetOp(Runtime::Instance::TableInstance &TabInst,
+                             const AST::TableInstruction &Instr);
+  Expect<void> runTableSetOp(Runtime::Instance::TableInstance &TabInst,
+                             const AST::TableInstruction &Instr);
+  Expect<void> runTableInitOp(Runtime::Instance::TableInstance &TabInst,
+                              Runtime::Instance::ElementInstance &ElemInst,
+                              const AST::TableInstruction &Instr);
+  Expect<void> runElemDropOp(Runtime::Instance::ElementInstance &ElemInst);
+  Expect<void> runTableCopyOp(Runtime::Instance::TableInstance &TabInst1,
+                              Runtime::Instance::TableInstance &TabInst2,
+                              const AST::TableInstruction &Instr);
+  Expect<void> runTableGrowOp(Runtime::Instance::TableInstance &TabInst);
+  Expect<void> runTableSizeOp(Runtime::Instance::TableInstance &TabInst);
+  Expect<void> runTableFillOp(Runtime::Instance::TableInstance &TabInst,
+                              const AST::TableInstruction &Instr);
   /// ======= Memory instructions =======
   template <typename T>
   TypeT<T> runLoadOp(Runtime::Instance::MemoryInstance &MemInst,
@@ -285,6 +309,14 @@ private:
                       const uint32_t BitWidth = sizeof(T) * 8);
   Expect<void> runMemorySizeOp(Runtime::Instance::MemoryInstance &MemInst);
   Expect<void> runMemoryGrowOp(Runtime::Instance::MemoryInstance &MemInst);
+  Expect<void> runMemoryInitOp(Runtime::Instance::MemoryInstance &MemInst,
+                               Runtime::Instance::DataInstance &DataInst,
+                               const AST::MemoryInstruction &Instr);
+  Expect<void> runDataDropOp(Runtime::Instance::DataInstance &DataInst);
+  Expect<void> runMemoryCopyOp(Runtime::Instance::MemoryInstance &MemInst,
+                               const AST::MemoryInstruction &Instr);
+  Expect<void> runMemoryFillOp(Runtime::Instance::MemoryInstance &MemInst,
+                               const AST::MemoryInstruction &Instr);
   /// ======= Test and Relation Numeric instructions =======
   template <typename T> TypeU<T> runEqzOp(ValVariant &Val) const;
   template <typename T>

lib/interpreter/engine/table.cpp
@@ -1,0 +1,142 @@
+// SPDX-License-Identifier: Apache-2.0
+#include "common/value.h"
+#include "interpreter/interpreter.h"
+#include "support/log.h"
+
+namespace SSVM {
+namespace Interpreter {
+
+Expect<void>
+Interpreter::runTableGetOp(Runtime::Instance::TableInstance &TabInst,
+                           const AST::TableInstruction &Instr) {
+  /// Pop Idx from Stack.
+  uint32_t Idx = retrieveValue<uint32_t>(StackMgr.pop());
+
+  /// Get table[Idx] and push to Stack.
+  if (auto Res = TabInst.getRefAddr(Idx)) {
+    StackMgr.push(*Res);
+  } else {
+    LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(), Instr.getOffset(),
+                                           {Idx},
+                                           {ValTypeFromType<uint32_t>()});
+    return Unexpect(Res);
+  }
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableSetOp(Runtime::Instance::TableInstance &TabInst,
+                           const AST::TableInstruction &Instr) {
+  /// Pop Ref from Stack.
+  ValVariant Ref = StackMgr.pop();
+
+  /// Pop Idx from Stack.
+  uint32_t Idx = retrieveValue<uint32_t>(StackMgr.pop());
+
+  /// Set table[Idx] with Ref.
+  if (auto Res = TabInst.setRefAddr(Idx, Ref); !Res) {
+    LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(), Instr.getOffset(),
+                                           {Idx},
+                                           {ValTypeFromType<uint32_t>()});
+    return Unexpect(Res);
+  }
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableInitOp(Runtime::Instance::TableInstance &TabInst,
+                            Runtime::Instance::ElementInstance &ElemInst,
+                            const AST::TableInstruction &Instr) {
+  /// Pop the length, source, and destination from stack.
+  uint32_t Len = retrieveValue<uint32_t>(StackMgr.pop());
+  uint32_t Src = retrieveValue<uint32_t>(StackMgr.pop());
+  uint32_t Dst = retrieveValue<uint32_t>(StackMgr.pop());
+
+  /// Replace tab[Dst : Dst + Len] with elem[Src : Src + Len].
+  if (auto Res = TabInst.setRefs(ElemInst.getRefs(), Dst, Src, Len)) {
+    return {};
+  } else {
+    LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(),
+                                           Instr.getOffset());
+    return Unexpect(Res);
+  }
+  return {};
+}
+
+Expect<void>
+Interpreter::runElemDropOp(Runtime::Instance::ElementInstance &ElemInst) {
+  /// Clear element instance.
+  ElemInst.clear();
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableCopyOp(Runtime::Instance::TableInstance &TabInst1,
+                            Runtime::Instance::TableInstance &TabInst2,
+                            const AST::TableInstruction &Instr) {
+  /// Pop the length, source, and destination from stack.
+  uint32_t Len = retrieveValue<uint32_t>(StackMgr.pop());
+  uint32_t Src = retrieveValue<uint32_t>(StackMgr.pop());
+  uint32_t Dst = retrieveValue<uint32_t>(StackMgr.pop());
+
+  /// Replace tab_1[Dst : Dst + Len] with tab_2[Src : Src + Len].
+  if (auto Refs = TabInst2.getRefs(Src, Len)) {
+    if (auto Res = TabInst1.setRefs(*Refs, Dst, 0, Len)) {
+      return {};
+    } else {
+      LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(),
+                                             Instr.getOffset());
+      return Unexpect(Res);
+    }
+  } else {
+    LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(),
+                                           Instr.getOffset());
+    return Unexpect(Refs);
+  }
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableGrowOp(Runtime::Instance::TableInstance &TabInst) {
+  /// Pop N for growing size, Val for init ref value.
+  uint32_t N = retrieveValue<uint32_t>(StackMgr.pop());
+  ValVariant &Val = StackMgr.getTop();
+
+  /// Grow size and push result.
+  const uint32_t CurrSize = TabInst.getSize();
+
+  if (TabInst.growTable(N, Val)) {
+    Val = CurrSize;
+  } else {
+    Val = static_cast<uint32_t>(-1);
+  }
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableSizeOp(Runtime::Instance::TableInstance &TabInst) {
+  /// Push SZ = size to stack.
+  StackMgr.push(TabInst.getSize());
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableFillOp(Runtime::Instance::TableInstance &TabInst,
+                            const AST::TableInstruction &Instr) {
+  /// Pop the length, ref_value, and offset from stack.
+  uint32_t Len = retrieveValue<uint32_t>(StackMgr.pop());
+  ValVariant Val = StackMgr.pop();
+  uint32_t Off = retrieveValue<uint32_t>(StackMgr.pop());
+
+  /// Fill refs with ref_value.
+  if (auto Res = TabInst.fillRefs(Val, Off, Len)) {
+    return {};
+  } else {
+    LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(),
+                                           Instr.getOffset());
+    return Unexpect(Res);
+  }
+}
+
+} // namespace Interpreter
+} // namespace SSVM

qdrvm/WasmEdge

include/interpreter/interpreter.h
@@ -244,6 +244,14 @@ private:
   /// Helper function for get global instance by index.
   Runtime::Instance::GlobalInstance *
   getGlobInstByIdx(Runtime::StoreManager &StoreMgr, const uint32_t Idx);
+
+  /// Helper function for get element instance by index.
+  Runtime::Instance::ElementInstance *
+  getElemInstByIdx(Runtime::StoreManager &StoreMgr, const uint32_t Idx);
+
+  /// Helper function for get data instance by index.
+  Runtime::Instance::DataInstance *
+  getDataInstByIdx(Runtime::StoreManager &StoreMgr, const uint32_t Idx);
   /// @}
 
   /// \name Run instructions functions
@@ -274,6 +282,22 @@ private:
                               const uint32_t Idx);
   Expect<void> runGlobalSetOp(Runtime::StoreManager &StoreMgr,
                               const uint32_t Idx);
+  /// ======= Table instructions =======
+  Expect<void> runTableGetOp(Runtime::Instance::TableInstance &TabInst,
+                             const AST::TableInstruction &Instr);
+  Expect<void> runTableSetOp(Runtime::Instance::TableInstance &TabInst,
+                             const AST::TableInstruction &Instr);
+  Expect<void> runTableInitOp(Runtime::Instance::TableInstance &TabInst,
+                              Runtime::Instance::ElementInstance &ElemInst,
+                              const AST::TableInstruction &Instr);
+  Expect<void> runElemDropOp(Runtime::Instance::ElementInstance &ElemInst);
+  Expect<void> runTableCopyOp(Runtime::Instance::TableInstance &TabInstDst,
+                              Runtime::Instance::TableInstance &TabInstSrc,
+                              const AST::TableInstruction &Instr);
+  Expect<void> runTableGrowOp(Runtime::Instance::TableInstance &TabInst);
+  Expect<void> runTableSizeOp(Runtime::Instance::TableInstance &TabInst);
+  Expect<void> runTableFillOp(Runtime::Instance::TableInstance &TabInst,
+                              const AST::TableInstruction &Instr);
   /// ======= Memory instructions =======
   template <typename T>
   TypeT<T> runLoadOp(Runtime::Instance::MemoryInstance &MemInst,
@@ -285,6 +309,14 @@ private:
                       const uint32_t BitWidth = sizeof(T) * 8);
   Expect<void> runMemorySizeOp(Runtime::Instance::MemoryInstance &MemInst);
   Expect<void> runMemoryGrowOp(Runtime::Instance::MemoryInstance &MemInst);
+  Expect<void> runMemoryInitOp(Runtime::Instance::MemoryInstance &MemInst,
+                               Runtime::Instance::DataInstance &DataInst,
+                               const AST::MemoryInstruction &Instr);
+  Expect<void> runDataDropOp(Runtime::Instance::DataInstance &DataInst);
+  Expect<void> runMemoryCopyOp(Runtime::Instance::MemoryInstance &MemInst,
+                               const AST::MemoryInstruction &Instr);
+  Expect<void> runMemoryFillOp(Runtime::Instance::MemoryInstance &MemInst,
+                               const AST::MemoryInstruction &Instr);
   /// ======= Test and Relation Numeric instructions =======
   template <typename T> TypeU<T> runEqzOp(ValVariant &Val) const;
   template <typename T>

lib/interpreter/engine/table.cpp
@@ -1,0 +1,142 @@
+// SPDX-License-Identifier: Apache-2.0
+#include "common/value.h"
+#include "interpreter/interpreter.h"
+#include "support/log.h"
+
+namespace SSVM {
+namespace Interpreter {
+
+Expect<void>
+Interpreter::runTableGetOp(Runtime::Instance::TableInstance &TabInst,
+                           const AST::TableInstruction &Instr) {
+  /// Pop Idx from Stack.
+  uint32_t Idx = retrieveValue<uint32_t>(StackMgr.pop());
+
+  /// Get table[Idx] and push to Stack.
+  if (auto Res = TabInst.getRefAddr(Idx)) {
+    StackMgr.push(*Res);
+  } else {
+    LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(), Instr.getOffset(),
+                                           {Idx},
+                                           {ValTypeFromType<uint32_t>()});
+    return Unexpect(Res);
+  }
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableSetOp(Runtime::Instance::TableInstance &TabInst,
+                           const AST::TableInstruction &Instr) {
+  /// Pop Ref from Stack.
+  ValVariant Ref = StackMgr.pop();
+
+  /// Pop Idx from Stack.
+  uint32_t Idx = retrieveValue<uint32_t>(StackMgr.pop());
+
+  /// Set table[Idx] with Ref.
+  if (auto Res = TabInst.setRefAddr(Idx, Ref); !Res) {
+    LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(), Instr.getOffset(),
+                                           {Idx},
+                                           {ValTypeFromType<uint32_t>()});
+    return Unexpect(Res);
+  }
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableInitOp(Runtime::Instance::TableInstance &TabInst,
+                            Runtime::Instance::ElementInstance &ElemInst,
+                            const AST::TableInstruction &Instr) {
+  /// Pop the length, source, and destination from stack.
+  uint32_t Len = retrieveValue<uint32_t>(StackMgr.pop());
+  uint32_t Src = retrieveValue<uint32_t>(StackMgr.pop());
+  uint32_t Dst = retrieveValue<uint32_t>(StackMgr.pop());
+
+  /// Replace tab[Dst : Dst + Len] with elem[Src : Src + Len].
+  if (auto Res = TabInst.setRefs(ElemInst.getRefs(), Dst, Src, Len)) {
+    return {};
+  } else {
+    LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(),
+                                           Instr.getOffset());
+    return Unexpect(Res);
+  }
+  return {};
+}
+
+Expect<void>
+Interpreter::runElemDropOp(Runtime::Instance::ElementInstance &ElemInst) {
+  /// Clear element instance.
+  ElemInst.clear();
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableCopyOp(Runtime::Instance::TableInstance &TabInstDst,
+                            Runtime::Instance::TableInstance &TabInstSrc,
+                            const AST::TableInstruction &Instr) {
+  /// Pop the length, source, and destination from stack.
+  uint32_t Len = retrieveValue<uint32_t>(StackMgr.pop());
+  uint32_t Src = retrieveValue<uint32_t>(StackMgr.pop());
+  uint32_t Dst = retrieveValue<uint32_t>(StackMgr.pop());
+
+  /// Replace tab_dst[Dst : Dst + Len] with tab_src[Src : Src + Len].
+  if (auto Refs = TabInstSrc.getRefs(Src, Len)) {
+    if (auto Res = TabInstDst.setRefs(*Refs, Dst, 0, Len)) {
+      return {};
+    } else {
+      LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(),
+                                             Instr.getOffset());
+      return Unexpect(Res);
+    }
+  } else {
+    LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(),
+                                           Instr.getOffset());
+    return Unexpect(Refs);
+  }
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableGrowOp(Runtime::Instance::TableInstance &TabInst) {
+  /// Pop N for growing size, Val for init ref value.
+  uint32_t N = retrieveValue<uint32_t>(StackMgr.pop());
+  ValVariant &Val = StackMgr.getTop();
+
+  /// Grow size and push result.
+  const uint32_t CurrSize = TabInst.getSize();
+
+  if (TabInst.growTable(N, Val)) {
+    Val = CurrSize;
+  } else {
+    Val = static_cast<uint32_t>(-1);
+  }
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableSizeOp(Runtime::Instance::TableInstance &TabInst) {
+  /// Push SZ = size to stack.
+  StackMgr.push(TabInst.getSize());
+  return {};
+}
+
+Expect<void>
+Interpreter::runTableFillOp(Runtime::Instance::TableInstance &TabInst,
+                            const AST::TableInstruction &Instr) {
+  /// Pop the length, ref_value, and offset from stack.
+  uint32_t Len = retrieveValue<uint32_t>(StackMgr.pop());
+  ValVariant Val = StackMgr.pop();
+  uint32_t Off = retrieveValue<uint32_t>(StackMgr.pop());
+
+  /// Fill refs with ref_value.
+  if (auto Res = TabInst.fillRefs(Val, Off, Len)) {
+    return {};
+  } else {
+    LOG(ERROR) << ErrInfo::InfoInstruction(Instr.getOpCode(),
+                                           Instr.getOffset());
+    return Unexpect(Res);
+  }
+}
+
+} // namespace Interpreter
+} // namespace SSVM